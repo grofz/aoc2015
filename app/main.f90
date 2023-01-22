2ef1a04aaad3c78e9484a7200efd8f6912e090dd
Zdenek Grof <grofz@vscht.cz>
2023-01-22 16:49:39 +0100

    Day 4 solved.

--- app/main.f90
+++ app/main.f90
@@ -1,9 +1,12 @@
 program main
   use day1501_mod, only : day1501
   use day1502_mod, only : day1502
+  use day1504_mod, only : day1504
   implicit none
 
+  goto 04
   01 call day1501('inp/01/input.txt')
   02 call day1502('inp/02/input.txt')
+  04 call day1504()
 
 end program main
---
+++ src/day1504.f90
@@ -0,0 +1,34 @@
+module day1504_mod
+  use day1605_mod, only : md5
+  implicit none
+
+contains
+
+  subroutine day1504()
+    character(len=*), parameter :: SALT = 'ckczppom'
+    integer :: i, ans1, ans2
+    integer, parameter :: MAX_TRY = 100000000
+    character(len=9) :: dig
+    character(len=32) :: hash
+
+    !print '(a)', md5('abcdef609043')
+    ans1 = 0
+    ans2 = 0
+    do i=1,MAX_TRY
+      write(dig,'(i0)') i
+      hash = md5(SALT//trim(adjustl(dig))) 
+      if (ans1==0 .and. hash(1:5)=='00000') then
+        ans1 = i
+      end if
+      if (ans2==0 .and. hash(1:6)=='000000') then
+        ans2 = i
+      end if
+      if (mod(i,100000)==0) print *, i
+      if (ans1/=0 .and. ans2/=0) exit
+    end do
+    if (i==MAX_TRY+1) error stop 'day 4 - not found'
+    print '("Answer 4/1 ",i0,l2)', ans1, ans1==117946
+    print '("Answer 4/2 ",i0,l2)', ans2, ans2==117946
+  end subroutine day1504
+
+end module day1504_mod
